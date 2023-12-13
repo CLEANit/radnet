4588380f0101fa05bc064a7ba9e4a7cb06b28841
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2023-12-13 13:06:12 -0500

    more work on figure

--- figures/hyperparameters/create_data_stability.py
+++ figures/hyperparameters/create_data_stability.py
@@ -10,6 +10,12 @@ def create_parser():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
+        "mode",
+        type=str,
+        choices=["rcut", "shape"],
+        help="Chooses which parameters varies between models.",
+    )
+    parser.add_argument(
         "pos_file",
         type=str,
         help="Path to the positions file.",
@@ -66,33 +72,67 @@ def read_value(prediction: str) -> np.array:
 
 def main(args):
     workdir = os.getcwd()
-
     n_models = 5
-    rcuts = ["1.5", "2.0", "2.5", "3.0", "3.5", "4.0"]
-    script_dir = os.path.dirname(__file__)
-    base_command = (
-        f"python {script_dir}/../../predict_raman.py {os.path.join(workdir, args.pos_file)} {args.prediction} "
-        f"--n_outputs {args.n_outputs} --sigma {args.sigma} --device {args.device} "
-        f"--image_shape {args.image_shape[0]} {args.image_shape[1]} {args.image_shape[2]} "
-        f"--save_results "
-    )
 
-    all_values = []
-    for rcut in rcuts:
-        all_values.append([])
-        for i in range(1, n_models + 1):
-            model_dir = args.saved_model_dir + f"{rcut}/{i}/"
-            os.chdir(model_dir)
+    if args.mode == "rcut":
+        rcuts = ["1.5", "2.0", "2.5", "3.0", "3.5", "4.0"]
+        script_dir = os.path.dirname(__file__)
+        base_command = (
+            f"python {script_dir}/../../predict_raman.py {os.path.join(workdir, args.pos_file)} {args.prediction} "
+            f"--n_outputs {args.n_outputs} --sigma {args.sigma} --device {args.device} "
+            f"--image_shape {args.image_shape[0]} {args.image_shape[1]} {args.image_shape[2]} "
+            f"--save_results "
+        )
 
-            model_name = "best.torch"
-            command = base_command + f"--rcut {rcut} --saved_model_path {model_name}"
-            out = subprocess.run(command.split(), capture_output=True, text=True)
-            if out.returncode == 0:
-                value = read_value(args.prediction)
-                all_values[-1].append(value)
-            else:
-                print("There was a problem with the subprocess!")
-                raise RuntimeError(out.stderr)
+        all_values = []
+        for rcut in rcuts:
+            all_values.append([])
+            for i in range(1, n_models + 1):
+                model_dir = args.saved_model_dir + f"{rcut}/{i}/"
+                os.chdir(model_dir)
+
+                model_name = "best.torch"
+                command = (
+                    base_command + f"--rcut {rcut} --saved_model_path {model_name}"
+                )
+                out = subprocess.run(command.split(), capture_output=True, text=True)
+                if out.returncode == 0:
+                    value = read_value(args.prediction)
+                    all_values[-1].append(value)
+                else:
+                    print("There was a problem with the subprocess!")
+                    raise RuntimeError(out.stderr)
+
+    elif args.mode == "shape":
+        shapes = ["6", "9", "12", "15", "18"]
+        script_dir = os.path.dirname(__file__)
+        base_command = (
+            f"python {script_dir}/../../predict_raman.py {os.path.join(workdir, args.pos_file)} {args.prediction} "
+            f"--n_outputs {args.n_outputs} --sigma {args.sigma} --device {args.device} "
+            f"--rcut {args.rcut} --save_results "
+        )
+
+        all_values = []
+        for shape in shapes:
+            all_values.append([])
+            for i in range(1, n_models + 1):
+                model_dir = args.saved_model_dir + f"{shape}/{i}/"
+                os.chdir(model_dir)
+
+                model_name = "best.torch"
+                command = (
+                    base_command
+                    + f"--image_shape {shape} {shape} {shape} --saved_model_path {model_name}"
+                )
+                out = subprocess.run(command.split(), capture_output=True, text=True)
+                if out.returncode == 0:
+                    value = read_value(args.prediction)
+                    all_values[-1].append(value)
+                else:
+                    print("There was a problem with the subprocess!")
+                    raise RuntimeError(out.stderr)
+    else:
+        raise RuntimeError("Run mode {args.mode} is not recognized.")
 
     os.chdir(workdir)
     all_values = np.array(all_values)
--- figures/hyperparameters/figure.py
+++ figures/hyperparameters/figure.py
@@ -6,65 +6,124 @@ import pickle
 
 data_dir = "data"
 inference_pol_rcut_path = os.path.join(data_dir, "BN_pol_inference_1.5_4.0.pkl")
-effch_data_path = os.path.join(data_dir, "BN_effch_1.5_4.0.npy")
+inference_die_rcut_path = os.path.join(data_dir, "BN_die_inference_1.5_4.0.pkl")
+rcut_effch_data_path = os.path.join(data_dir, "BN_effch_1.5_4.0.npy")
+rcut_suscept_data_path = os.path.join(data_dir, "BN_suscept_1.5_4.0.npy")
 
-effch_data = np.load(effch_data_path)
+rcut_effch_data = np.load(rcut_effch_data_path)
+rcut_suscept_data = np.load(rcut_suscept_data_path)
 with open(inference_pol_rcut_path, "rb") as f:
     inf_pol_rcut_data = pickle.load(f)
+with open(inference_die_rcut_path, "rb") as f:
+    inf_die_rcut_data = pickle.load(f)
 
 
-inf_pol_rcut_means = np.mean(inf_pol_rcut_data["maes"][1:], axis=1)
-inf_pol_rcut_stds = np.std(inf_pol_rcut_data["maes"][1:], axis=1)
+inf_pol_rcut_means = np.mean(inf_pol_rcut_data["maes"], axis=1)
+inf_pol_rcut_stds = np.std(inf_pol_rcut_data["maes"], axis=1)
+inf_die_rcut_means = np.mean(inf_die_rcut_data["maes"], axis=1)
+inf_die_rcut_stds = np.std(inf_die_rcut_data["maes"], axis=1)
 
-effch_data = effch_data.reshape(effch_data.shape[0], effch_data.shape[1], -1)[1:]
-effch_stds = np.mean(np.std(effch_data, axis=1), axis=1)
+rcut_effch_data = rcut_effch_data.reshape(
+    rcut_effch_data.shape[0], rcut_effch_data.shape[1], -1
+)[1:]
+rcut_effch_stds = np.mean(np.std(rcut_effch_data, axis=1), axis=1)
+rcut_suscept_data = rcut_suscept_data.reshape(
+    rcut_suscept_data.shape[0], rcut_suscept_data.shape[1], -1
+)[1:]
+rcut_suscept_stds = np.mean(np.std(rcut_suscept_data, axis=1), axis=1)
 
 
-
-
-rcuts = [2.0, 2.5, 3.0, 3.5, 4.0]
+rcuts = [1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
 
 font = {"family": "CMU Serif", "size": 18}
 plt.rc("font", **font)
 plt.rc("text", usetex=True)
 plt.rc("text.latex", preamble=r"\usepackage{bm}")
+plt.rcParams["axes.formatter.limits"] = (0, 0)
 
 fig = plt.figure(figsize=(16, 14))
 gs = fig.add_gridspec(nrows=2, ncols=2, left=0.06, right=0.90, hspace=0.0, wspace=0.20)
-#fig, ax = plt.subplots(nrows=2, ncols=2)
-#(ax1, ax2), (ax3, ax4) = ax
 
-ax1 = fig.add_subplot(gs[0,0])
+ax1 = fig.add_subplot(gs[0, 0])
+ax1.set_title("Cutoff convergence", fontsize=25)
 pol1 = ax1.errorbar(rcuts, inf_pol_rcut_means, yerr=inf_pol_rcut_stds, color="b")
 ax1.grid(linestyle="-.")
-ax1.set_ylim([0, None])
 ax1.set_ylabel("Test MAE")
 ax1.set_xlabel("Cutoff radius (\AA)")
+ax1.set_yscale("log")
+ax1.spines["left"].set_edgecolor(pol1[0].get_color())
+ax1.tick_params(axis="y", which="both", colors=pol1[0].get_color())
 ax2 = ax1.twinx()
-die2 = ax2.errorbar(rcuts, np.linspace(0,1, len(rcuts)), yerr=0.1, color="r")
-
-#print(type(pol1[0]), type(die2[0]))
+die2 = ax2.errorbar(rcuts, inf_die_rcut_means, yerr=inf_die_rcut_stds, color="r")
+ax2.set_yscale("log")
+ax2.spines["right"].set_edgecolor(die2[0].get_color())
+ax2.tick_params(axis="y", which="both", colors=die2[0].get_color())
 
 ax1.legend([pol1, die2], ["Polarization", "Dielectric"])
 
-ax3 = fig.add_subplot(gs[0,1])
+
+ax3 = fig.add_subplot(gs[0, 1])
 ax3.grid(linestyle="-.")
 ax3.set_ylim([0, None])
-ax3.set_ylabel("Test MAE")
-ax3.yaxis.set_label_position("right")
 ax3.yaxis.tick_right()
 
-ax5 = fig.add_subplot(gs[1,0])
-ax5.plot(rcuts, effch_stds, label="Effective charges (e)")
+ax4 = ax3.twinx()
+ax4.set_ylabel("Test MAE")
+ax4.set_xlim(ax3.get_xlim())
+
+ax5 = fig.add_subplot(gs[1, 0])
+eff5 = ax5.plot(
+    rcuts[1:], rcut_effch_stds, label="Effective charges (e)", color=pol1[0].get_color()
+)
 ax5.grid(linestyle="-.")
 ax5.set_ylim([0, None])
 ax5.set_xlabel("Cutoff radius (\AA)")
-ax5.set_ylabel("Derivatives STD (e)")
+ax5.set_ylabel("Derivatives STD")
+ax5.set_xlim(ax1.get_xlim())
+ax5.spines["left"].set_edgecolor(eff5[0].get_color())
+ax5.tick_params(axis="y", which="both", colors=eff5[0].get_color())
+ax5.yaxis.set_ticks([0, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035])
+ax5.yaxis.set_ticklabels(["0.0", "0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5"])
+fig.text(
+    ax5.get_position().get_points()[0, 0] - 0.04,
+    ax5.get_position().get_points()[1, 1] - 0.015,
+    r"$\times 10^{-2}$",
+    color=eff5[0].get_color(),
+)
 
-ax6 = fig.add_subplot(gs[1,1])
-ax6.grid(linestyle="-.")
+
+ax6 = ax5.twinx()
+sus6 = ax6.plot(
+    rcuts[1:],
+    rcut_suscept_stds,
+    color=die2[0].get_color(),
+)
 ax6.set_ylim([0, None])
-ax6.set_xlabel("Image Shape")
+ax6.yaxis.set_ticks([0, 0.00025, 0.0005, 0.00075, 0.001, 0.00125, 0.0015, 0.00175])
+ax6.yaxis.set_ticklabels(["0.0", "2.5", "5.0", "7.5", "10.0", "12.5", "15.0", "17.5"])
+fig.text(
+    ax6.get_position().get_points()[1, 0] + 0.005,
+    ax6.get_position().get_points()[1, 1] - 0.015,
+    r"$\times 10^{-4}$",
+    color=sus6[0].get_color(),
+)
+ax6.spines["right"].set_edgecolor(sus6[0].get_color())
+ax6.tick_params(axis="y", which="both", colors=sus6[0].get_color())
+ax6.yaxis.label.set_color(sus6[0].get_color())
+ax5.legend(
+    [eff5[0], sus6[0]],
+    [r"$\Delta Z^{*}$", r"$\Delta\frac{\partial \chi}{\partial R}$"],
+    loc="lower left",
+)
+
+ax7 = fig.add_subplot(gs[1, 1])
+ax7.grid(linestyle="-.")
+ax7.set_ylim([0, None])
+ax7.set_xlabel("Image Shape")
+
+ax8 = ax7.twinx()
+ax8.set_xlabel(ax7.get_xlabel())
+ax8.set_ylabel("Derivatives STD")
 
 plt.show()
-#plt.savefig("fig.pdf")
+# plt.savefig("fig.pdf")
